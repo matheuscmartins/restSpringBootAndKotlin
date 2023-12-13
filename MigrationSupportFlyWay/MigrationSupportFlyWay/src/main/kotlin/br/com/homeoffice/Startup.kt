6060cf8b972a8939fdb6496fb322590d408b7c83
Matheus Cruz Martins <matheus.martins@neogrid.com>
2023-12-12 21:10:43 -0300

    flyway migration

--- MigrationSupportFlyWay/MigrationSupportFlyWay/src/main/kotlin/br/com/homeoffice/Startup.kt
+++ MigrationSupportFlyWay/MigrationSupportFlyWay/src/main/kotlin/br/com/homeoffice/Startup.kt
@@ -10,4 +10,8 @@ fun main(args: Array<String>) {
 	runApplication<Startup>(*args)
 	//http://localhost:8080/sum/1/2
 	// sum, subtraction, division, multiplication or mean
+
+	// mvn clean package spring-boot:run -DskipTests
+
+	//mvn flyway:migrate
 }
--- MigrationSupportFlyWay/MigrationSupportFlyWay/src/main/kotlin/br/com/homeoffice/controller/PersonController.kt
+++ MigrationSupportFlyWay/MigrationSupportFlyWay/src/main/kotlin/br/com/homeoffice/controller/PersonController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 class PersonController {
 
 
